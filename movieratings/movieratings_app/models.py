8781e9257983753bfb955cd9e33b2adcb80e08f3
Will Warren <will.warren04@gmail.com>
2017-03-16 20:24:26 -0400

    load movies working, load_raters and load_ratings next

--- movieratings/movieratings_app/models.py
+++ movieratings/movieratings_app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    movie_id = models.IntegerField()
     title = models.CharField(max_length=100)
     release_date = models.CharField(max_length=15)
     imdb_link = models.CharField(max_length=100)
@@ -18,22 +17,21 @@ class Movie(models.Model):
         pass
 
 
-class Rating(models.Model):
-    rater_id = models.IntegerField()
-    movie_id = models.IntegerField()
-    rating = models.IntegerField()
-    timestamp = models.IntegerField()
-
-
 class Rater(models.Model):
-    rater_id = models.IntegerField()
-    age = models.IntegerField()
+    age = models.IntegerField() # invalid literal
     gender = models.CharField(max_length=1)
     occupation = models.CharField(max_length=20)
-    zip_code = models.IntegerField()
+    zip_code = models.CharField(max_length=10)
 
     def get_top_n_unseen_movies():
         pass
 
     def get_total_avg_ratings():
         pass
+
+
+class Rating(models.Model):
+    rater = models.ForeignKey(Rater, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    timestamp = models.IntegerField()
